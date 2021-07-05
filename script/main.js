a3c8ef7d34035979ca2be5389e333b9b784cf335
zeeckcwb <ezqlcwb@hotmail.com>
2021-07-05 16:11:15 -0300

    Inserção da mascara no valor total após o cálculo, correção que apresenta lucro ou prejuizo

--- script/main.js
+++ script/main.js
@@ -1,69 +1,69 @@
-$(document).ready(function(){
-    $('#valor').mask('000.000.000.000.000,00', {reverse: true});
+$(document).ready(function () {
+    $('#valor').mask('000.000.000.000.000,00', { reverse: true });
 });
 
 var transacao = [];
 
-function validarCampo(e){
+function validarCampo(e) {
     e.preventDefault()
     var error = false;
 
     var tipo = document.getElementById("tipo").value;
     var validacaoTipo = document.getElementById("validacaoTipo");
-    if(tipo == ""){
+    if (tipo == "") {
         var error = true;
         validacaoTipo.innerHTML = "Selecione 'Compra ou Venda'";
     }
 
     var mercadoria = document.getElementById("mercadoria").value;
     var validacaoMercadoria = document.getElementById("validacaoMercadoria");
-    if(mercadoria == ""){
+    if (mercadoria == "") {
         var error = true;
         validacaoMercadoria.innerHTML = "Preencha o campo mercadoria";
     }
 
     var valor = document.getElementById("valor").value;
     var validacaoValor = document.getElementById("validacaoValor");
-    if(valor == ""){
+    if (valor == "") {
         var error = true;
         validacaoValor.innerHTML = "Preencha o campo valor";
     }
 
-    if(!error){
-        if(transacao == null){
+    if (!error) {
+        if (transacao == null) {
             transacao = []
         }
-        transacao.push({tipo: tipo, mercadoria: mercadoria, valor: valor})
+        transacao.push({ tipo: tipo, mercadoria: mercadoria, valor: valor })
         localStorage.setItem('transacao', JSON.stringify(transacao))
         listarTransacoes()
     }
 }
 
-function listarTransacoes(){
+function listarTransacoes() {
     transacao = JSON.parse(localStorage.getItem('transacao'))
-    if(transacao != null){
+    if (transacao != null) {
         document.querySelector('#content-table').innerHTML = transacao.map((trsc) => {
-        return (
-            `<tr>
+            return (
+                `<tr>
                 <td class='calc_symbol'>+</td>
-                <td class='left'>`+trsc.mercadoria+`</td>
-                <td class='right'>R$ `+trsc.valor+`</td>
+                <td class='left'>`+ trsc.mercadoria + `</td>
+                <td class='right'>R$ `+ trsc.valor + `</td>
             </tr>
             <tr>
                 <td colspan="3" class="border"></td>
             </tr>`
-        )
-    }).join('')
-    alterarSimbolo()
-    listarTotal()
+            )
+        }).join('')
+        alterarSimbolo()
+        listarTotal()
     }
 }
 
-function alterarSimbolo(){
+function alterarSimbolo() {
     transacao = JSON.parse(localStorage.getItem('transacao'))
     i = 0;
-    for(; i < transacao.length; i++){
-        if(transacao[i].tipo != "Compra"){
+    for (; i < transacao.length; i++) {
+        if (transacao[i].tipo != "Compra") {
             document.getElementsByClassName('calc_symbol')[i].innerHTML = "+"
         } else {
             document.getElementsByClassName('calc_symbol')[i].innerHTML = "-"
@@ -71,14 +71,14 @@ function alterarSimbolo(){
     }
 }
 
-function deletarTransacoes(){
+function deletarTransacoes() {
     localStorage.removeItem('transacao')
     alert("registros excluidos")
     listarTransacoes()
 }
 
 var total = 0
-function calculoValores(){
+function calculoValores() {
     let transacao = JSON.parse(localStorage.getItem('transacao'))
     let totalStrVenda = []
     let totalStrCompra = []
@@ -88,16 +88,16 @@ function calculoValores(){
     let totalCompra = 0
     let i = 0
     let j = 0
-    if(transacao != null){
-        for(; i < transacao.length; i++){
-            if(transacao[i].tipo == "Compra"){
+    if (transacao != null) {
+        for (; i < transacao.length; i++) {
+            if (transacao[i].tipo == "Compra") {
                 totalStrCompra = [transacao[i].valor.replace(/\D/g, '')]
                 totalNbrCompra = Number.parseFloat(totalStrCompra)
                 totalCompra += totalNbrCompra
             }
         }
-        for(; j < transacao.length; j++){
-            if(transacao[j].tipo == "Venda"){
+        for (; j < transacao.length; j++) {
+            if (transacao[j].tipo == "Venda") {
                 totalStrVenda = [transacao[j].valor.replace(/\D/g, '')]
                 totalNbrVenda = Number.parseFloat(totalStrVenda)
                 totalVenda += totalNbrVenda
@@ -107,18 +107,31 @@ function calculoValores(){
     }
 }
 
-function listarTotal(){
+function listarTotal() {
     calculoValores()
+    formatarMoeda()
     var campoTotal = document.getElementById('campoTotal')
-    campoTotal.innerHTML = total;
-    
-    if(total > 0){
+    campoTotal.innerHTML = "R$ " + totalFormatado;
+
+    if (total > 0) {
         campoLucro.innerHTML = "[LUCRO]"
     }
-    if(total < 0){
+    else if (total < 0) {
         campoLucro.innerHTML = "[PREJUIZO]"
-    } 
-    else{
+    }
+    else {
         campoLucro.innerHTML = ""
+    }
+}
+
+function formatarMoeda() {
+    totalFormatado = total
+    totalFormatado = totalFormatado + '';
+    totalFormatado = parseInt(totalFormatado.replace(/[\D]+/g, ''));
+    totalFormatado = totalFormatado + '';
+    totalFormatado = totalFormatado.replace(/([0-9]{2})$/g, ",$1");
+
+    if (totalFormatado.length > 6) {
+        totalFormatado = totalFormatado.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
     }
 }
